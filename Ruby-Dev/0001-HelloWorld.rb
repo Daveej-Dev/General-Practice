a57bb20a4cea57273b9ae6d391d8776a2953b055
Daveej-Dev <daveejdev1@gmail.com>
2022-10-12 09:36:50 -0600

    First Ruby program!

---
+++ Ruby-Dev/0001-HelloWorld.rb
@@ -0,0 +1,4 @@
+#Let's start simple... this is a program that outputs the phrase "Hello World!" to the terminal window.
+puts "Hello World!"
+#The "puts" statement above tells the computer to write something to the terminal window. This is what's called a function.
+#The "Hello World!" in quotations tells the puts statement what to display in the terminal. This is what's called a parameter.
